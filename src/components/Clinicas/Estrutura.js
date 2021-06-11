804d690b53595acaf3c5a4d73f0deee77bb24603
Ayratan <70165987+Ayratan@users.noreply.github.com>
2021-06-11 10:18:15 -0300

    primeiro backup

---
+++ src/components/Clinicas/Estrutura.js
@@ -0,0 +1,43 @@
+import React from "react";
+import './Estrutura.css'
+
+const ClinicasEstrutura = ({clinica}) => (
+
+<div className="estrutura-clinica">
+        <span>Clinica</span>
+         <div className="estrutura-clinica__info">
+        
+        
+         <p>{clinica.nome}</p>
+         <div className="estrutura-clinica_endemail"> 
+         <p>{clinica.endereço}</p>
+         <p>{clinica.email}</p>
+        
+         </div>
+         </div>
+        <div className="estrutura-clinica__exames">
+            {clinica.exclin === true &&(
+                <li>Exame Clínico</li>
+            )}
+            {clinica.excomp === true &&(
+                <li>Exame Complementar</li>
+            )}
+            {clinica.PPRA === true &&(
+                <li>PPRA</li>
+            )}
+            {clinica.PCMSO === true &&(
+                <li>PCMSO</li>
+            )}
+            
+           
+            
+        </div >
+        <div className="estrutura_clinica__whats" >
+ 
+           Whatsapp:
+        <p >{clinica.whatsapp}</p>
+        </div>
+          </div>
+)
+
+export default ClinicasEstrutura;
